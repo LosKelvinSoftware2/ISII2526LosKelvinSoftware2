b79e9fb8508e034168333160e6220f2fc6d1e810
JaviTron2004 <javier.mellado1@alu.uclm.es>
2025-10-03 16:14:43 +0200

    He añadido información a las tablas (#11)

--- src/AppForSEII2526.API/Models/CompraItem.cs
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -1,6 +1,21 @@
 ﻿namespace AppForSEII2526.API.Models
 {
+    [PrimaryKey(nameof(herramientaId), nameof(compraId))]
     public class CompraItem
     {
+
+        public int cantidad { get; set; }
+        public string descripcion { get; set; }
+        public float precio { get; set; }
+
+        // Foreign Keys (Conexión a base de datos)
+        public Compra compra { get; set; }
+        public int compraId { get; set; }
+
+        public Herramienta herramienta { get; set; }
+        public int herramientaId { get; set; }
+
+
+
     }
 }
--- src/AppForSEII2526.API/Models/Herramienta.cs
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -2,6 +2,20 @@
 {
     public class Herramienta
     {
+        public int Id { get; set; }
+        public string material { get; set; }
+        public string nombre { get; set; }
+        public float precio { get; set; }
+        public string tiempoReparacion { get; set; }
+
+
+
+        public List<OfertaItem> Ofertaitems { get; set; }
+        public Fabricante Fabricante { get; set; }
+        public int FabricanteId { get; set; }
+        public List<CompraItem> CompraItems { get; set; }
+
+
 
 
     }
