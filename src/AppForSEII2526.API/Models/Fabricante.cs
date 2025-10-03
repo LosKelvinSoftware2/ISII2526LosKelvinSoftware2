6798f3e3a241181c2c3774af6f8edf888159ce33
Javier Saelices López-Gasco <javier.saelices@alu.uclm.es>
2025-10-03 16:31:39 +0200

    Creación de mis tablas (#13)
    Creacion de mis tablas

--- src/AppForSEII2526.API/Models/Fabricante.cs
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -4,6 +4,9 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        //Conexiones otras tablas
+        public List<Herramienta> herramientas { get; set; }
+
 
 
     }
---
+++ src/AppForSEII2526.API/Models/ReparacionItem.cs
@@ -0,0 +1,16 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(herramientaId), nameof(IdReparacion))]
+    public class ReparacionItem
+    {
+        public float precio { get; set; }
+        public string descripcion { get; set; }
+        public int cantidad { get; set; }
+
+        //Conexiones otras tablas
+        public Herramienta herramienta { get; set; }
+        public int herramientaId { get; set; }
+        public Reparacion IdReparacion { get; set; }
+        public int reparacion { get; set; }
+    }
+}
