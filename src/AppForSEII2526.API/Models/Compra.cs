42bf9285cd4f3f1d1c27560f5a868807e3d2a223
JAVIER MELLADO MONTEJANO <Javier.Mellado1@alu.uclm.es>
2025-10-03 16:13:20 +0200

    He añadido información a las tablas

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -2,5 +2,19 @@
 {
     public class Compra
     {
+        public string apellidoCliente { get; set; }
+        public string nombreCliente { get; set; }
+        public string correoelectronico { get; set; }
+        public string direccionEnvio { get; set; }
+        public string fechacompra { get; set; }
+        public int Id { get; set; }
+        public float precioTotal { get; set; }
+        public double telefono { get; set; }
+
+
+        public List<CompraItem> CompraItems { get; set; }
+        public tiposMetodoPago MetodoPago { get; set; }
+
+
     }
 }
--- src/AppForSEII2526.API/Models/Fabricante.cs
+++ src/AppForSEII2526.API/Models/Fabricante.cs
@@ -2,5 +2,9 @@
 {
     public class Fabricante
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+
     }
 }
