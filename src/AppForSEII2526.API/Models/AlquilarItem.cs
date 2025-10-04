84cb22505b028b56124751637a901d0c72764f82
JuanPedroSerrano <juanpedro.serrano1@alu.uclm.es>
2025-10-04 12:02:12 +0200

    Creación clases AlquilarItem y Alquiler (#19)

---
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -0,0 +1,15 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(herramientaId), nameof(alquilerId))]
+    public class AlquilarItem
+    {
+        public int cantidad { get; set; }
+        public int precio { get; set; }
+        //FK hacia Alquiler
+        public int alquilerId { get; set; }
+        public Alquiler alquiler { get; set; }
+        //FK hacia Herramienta
+        public int herramientaId { get; set; }
+        public Herramienta herramienta { get; set; }
+    }
+}
