e3af8bee3a66723163fd50b4dc1aa697b58d9d41
JAVIER SAELICES LÓPEZ GASCO <Javier.Saelices@alu.uclm.es>
2025-10-17 19:53:00 +0200

    El sistema muestra las reparaciones solicitadas, indicando los datos del cliente (Nombre y Apellidos), fecha de entrega, fecha de recogida, su precio total, y las herramientas a reparar (nombre, precio, descripción del problema y cantidad).

--- src/AppForSEII2526.API/Controllers/HerramientasController.cs
+++ src/AppForSEII2526.API/Controllers/HerramientasController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AppForSEII2526.API.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -17,6 +19,55 @@ namespace AppForSEII2526.API.Controllers
             _logger = logger;
 
         }
+
+        //Reparacion de herramientas Saelices
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReparacionDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetReparacion(int id)
+        {
+            if (_context.Reparacion == null)
+            {
+                _logger.LogError("Error: Reparaciones table does not exist");
+                return NotFound();
+            }
+
+            var reparacion = await _context.Reparacion
+                .Where(r => r.Id == id)
+                .Include(r => r.ItemsReparacion)         // join con ReparacionItem
+                    .ThenInclude(ri => ri.Herramienta)   // join con Herramienta
+                .Select(r => new ReparacionDTO(
+                    r.Id,
+                    r.Cliente,                           // Nombre completo del cliente
+                    r.FechaEntrega,
+                    r.FechaRecogida,
+                    r.PrecioTotal,
+                    r.ItemsReparacion
+                        .Select(ri => new ReparacionItemDTO(
+                            ri.Cantidad,
+                            ri.Descripcion,
+                            ri.Herramienta.Precio,
+                            ri.Herramienta.Id,
+                            ri.Herramienta,
+                            ri.reparacionId,
+                            ri.Reparacion
+
+                        )).ToList()
+                    , r.MetodoPago
+                ))
+                .FirstOrDefaultAsync();
+
+            if (reparacion == null)
+            {
+                _logger.LogWarning($"No se encontró la reparación con id {id}");
+                return NotFound();
+            }
+
+            return Ok(reparacion);
+        }
+
+
 
 
 
---
+++ src/AppForSEII2526.API/DTO/ReparacionDTO.cs
@@ -0,0 +1,44 @@
+﻿namespace AppForSEII2526.API.DTO
+{
+    public class ReparacionDTO
+    {
+        private object value;
+
+        
+        public ReparacionDTO(int id, ApplicationUser cliente, DateTime fechaEntrega, DateTime fechaRecogida, float precioTotal, List<ReparacionItemDTO> itemsReparacion, tiposMetodoPago metodoPago)
+        {
+            Id = id;
+            Cliente = cliente;
+            FechaEntrega = fechaEntrega;
+            FechaRecogida = fechaRecogida;
+            PrecioTotal = precioTotal;
+            ItemsReparacion = itemsReparacion;
+            MetodoPago = metodoPago;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        //Vinculación con el usuario (cliente autenticado)
+        [Required]
+        public ApplicationUser Cliente { get; set; }
+
+
+        [Required]
+        public DateTime FechaEntrega { get; set; } // No puede ser anterior a hoy
+
+        [Required]
+        public DateTime FechaRecogida { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue)]
+        public float PrecioTotal { get; set; }
+
+        // Relación con los ítems de reparación
+        public List<ReparacionItemDTO> ItemsReparacion { get; set; }
+
+        // Método de pago obligatorio
+        [Required]
+        public tiposMetodoPago MetodoPago { get; set; }
+    }
+}
