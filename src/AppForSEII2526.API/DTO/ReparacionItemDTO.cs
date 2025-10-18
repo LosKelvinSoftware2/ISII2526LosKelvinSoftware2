3f19ed6d6a184e504ba2b5f0c04422bd0aff854f
Javier Saelices López-Gasco <javier.saelices@alu.uclm.es>
2025-10-18 12:29:38 +0200

    El sistema muestra las reparaciones solicitadas (#46)
    El sistema muestra las reparaciones solicitadas, indicando los datos del cliente
    (Nombre y Apellidos), fecha de entrega, fecha de recogida, su precio total, y las
    herramientas a reparar (nombre, precio, descripción del problema y cantidad).

---
+++ src/AppForSEII2526.API/DTO/ReparacionItemDTO.cs
@@ -0,0 +1,33 @@
+﻿namespace AppForSEII2526.API.DTO
+{
+    public class ReparacionItemDTO
+    {
+        public ReparacionItemDTO(int cantidad, string descripcion, float precio, int herramientaId, Herramienta herramienta, int reparacionId, Reparacion reparacion)
+        {
+            Cantidad = cantidad;
+            Descripcion = descripcion;
+            Precio = precio;
+            this.herramientaId = herramientaId;
+            Herramienta = herramienta;
+            this.reparacionId = reparacionId;
+            Reparacion = reparacion;
+        }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser al menos 1.")]
+        public int Cantidad { get; set; }
+        // Campo opcional
+        public string Descripcion { get; set; }
+        [Required]
+        [Range(0, float.MaxValue)]
+        public float Precio { get; set; }
+
+        // Relación con Herramienta
+        public int herramientaId { get; set; }
+        public Herramienta Herramienta { get; set; }
+
+        // Relación con Reparacion
+        public int reparacionId { get; set; }
+        public Reparacion Reparacion { get; set; }
+    }
+}
