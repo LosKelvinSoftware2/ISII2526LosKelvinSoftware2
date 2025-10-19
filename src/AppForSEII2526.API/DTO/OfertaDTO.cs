af2252fada0e607d1763e7e08f5e93f5516397e4
telmocho <telmo.garcia@alu.uclm.es>
2025-10-19 15:20:17 +0200

    Método get del CU-Crear Oferta implementado. También, DTOs OfertaDTO … (#49)
    Método get del CU-Crear Oferta implementado. También, DTOs OfertaDTO y OfertaItemDTO implementados.
    
    He añadido también en el constructor de HerramientaDTO el atributo Fabricante.
    
    Co-authored-by: garci <garci@TELMO_PC>

---
+++ src/AppForSEII2526.API/DTO/OfertaDTO.cs
@@ -0,0 +1,39 @@
+﻿namespace AppForSEII2526.API.DTO
+{
+    public class OfertaDTO
+    {
+        private object value;
+        private Func<List<OfertaItemDTO>> toList;
+
+        public OfertaDTO(int id, string fechaFinal, string fechaInicio, string fechaOferta, Func<List<OfertaItemDTO>> toList, tiposMetodoPago metodoPago, tiposDiridaOferta? dirigidaA)
+        {
+            Id = id;
+            this.fechaFinal = fechaFinal;
+            this.fechaInicio = fechaInicio;
+            this.fechaOferta = fechaOferta;
+            this.toList = toList;
+            this.metodoPago = metodoPago;
+            this.dirigidaA = dirigidaA;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public String fechaFinal { get; set; }
+
+        [Required]
+        public String fechaInicio { get; set; } // No puede ser anterior a hoy
+        public String fechaOferta { get; set; } // Debe ser después de la fecha de inicio y antes de la fecha final
+
+        // Conexiones otras tablas
+        public List<OfertaItem> ofertaItems { get; set; }
+
+        [Required]
+        public tiposMetodoPago metodoPago { get; set; }
+
+        // La interrogación significa que puede ser nulo
+        public tiposDiridaOferta? dirigidaA { get; set; }
+    }
+}
+
---
+++ src/AppForSEII2526.API/DTO/OfertaItemDTO.cs
@@ -0,0 +1,40 @@
+﻿namespace AppForSEII2526.API.DTO
+{
+    public class OfertaItemDTO
+    {
+
+        public OfertaItemDTO(float precioFinal, string nombre, string material,
+            string fabricante, float precioOriginal)
+        {
+            this.precioFinal = precioFinal;
+            this.nombre = nombre;
+            this.material = material;
+            this.fabricante = fabricante;
+            this.precioOriginal = precioOriginal; // Viene de Herramienta.
+            // Viene de OfertaItem.precioFinal (float se recomienda cambiar a decimal) 
+        }
+
+        
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "El precio total no puede ser negativo.")]
+        public float precioFinal { get; set; }
+
+        string fabricante { get; set; }
+
+        float precioOriginal;
+
+        string nombre { get; set; }
+
+        string material { get; set; }
+
+
+
+        // FK hacia Herramienta
+        public int herramientaId { get; set; }
+        public Herramienta herramienta { get; set; }
+
+        // FK hacia Reparacion
+        public int ofertaId { get; set; }
+        public Oferta oferta { get; set; }
+    }
+}
