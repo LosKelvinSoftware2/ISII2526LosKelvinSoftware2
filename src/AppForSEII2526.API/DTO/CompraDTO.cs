2a712e83eab2471ec3f547fa62bfcb2733fc91f2
JaviTron2004 <javier.mellado1@alu.uclm.es>
2025-10-21 16:01:28 +0200

    DTOS y herramienta controller (#53)

---
+++ src/AppForSEII2526.API/DTO/CompraDTO.cs
@@ -0,0 +1,43 @@
+﻿namespace AppForSEII2526.API.DTO
+{
+    public class CompraDTO
+    {
+
+        public CompraDTO(int Id, ApplicationUser Cliente, string direccionEnvio, DateTime fechaCompra, float PrecioTotal, 
+            List<CompraItemDTO> CompraItemDTO, tiposMetodoPago MetodoPago)
+        {
+            this.Id = Id;
+            this.Cliente = Cliente;
+            this.direccionEnvio = direccionEnvio;
+            this.fechaCompra = fechaCompra;
+            this.PrecioTotal = PrecioTotal;
+            this.CompraItemsDTO = CompraItemDTO;
+            this.MetodoPago = MetodoPago;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        //Vinculación con el usuario (cliente autenticado)
+        [Required]
+        public ApplicationUser Cliente { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string direccionEnvio { get; set; }
+
+        [Required]
+        public DateTime fechaCompra { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "El precio total no puede ser negativo.")]
+        public float PrecioTotal { get; set; }
+
+        [Required]
+        public tiposMetodoPago MetodoPago { get; set; }
+
+        public List<CompraItemDTO> CompraItemsDTO { get; set; }
+
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTO/CompraItemDTO.cs
@@ -0,0 +1,43 @@
+﻿namespace AppForSEII2526.API.DTO
+{
+    public class CompraItemDTO
+    {
+        public CompraItemDTO(int cantidad, string descripcion, float precio,
+            int herramientaId, Herramienta herramienta, int compraId, Compra compra) {
+
+            this.cantidad = cantidad;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.herramientaId = herramientaId;
+            this.herramienta = herramienta;
+            this.compraId = compraId;
+            this.compra = compra;
+
+        }
+
+      
+
+        [Required]
+        public int cantidad { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string descripcion { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "El precio no puede ser negativo.")]
+        public float precio { get; set; }
+
+        // Foreign Keys (Conexión a base de datos)
+
+        public int compraId { get; set; }
+        public Compra compra { get; set; }
+
+        public int herramientaId { get; set; }
+        public Herramienta herramienta { get; set; }
+
+
+
+
+    }
+}
