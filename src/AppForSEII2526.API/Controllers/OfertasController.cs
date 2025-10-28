c68faca68df617b74d8b46036e2bbdc3e4021364
telmocho <telmo.garcia@alu.uclm.es>
2025-10-28 16:45:47 +0100

    66 corregir get details y añadirlo a un nuevo controlador (#69)
    He organizado mis dtos en nuevas carpetas y he añadido el Get Select en HerramientasController
    
    Co-authored-by: garci <garci@TELMO_PC>
    Co-authored-by: JaviTron2004 <javier.mellado1@alu.uclm.es>

---
+++ src/AppForSEII2526.API/Controllers/OfertasController.cs
@@ -0,0 +1,65 @@
+﻿using AppForSEII2526.API.DTO.OfertaDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OfertasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; 
+        private readonly ILogger<OfertasController> _logger;
+
+        public OfertasController(ApplicationDbContext context, ILogger<OfertasController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(OfertaDetailsDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetOferta(int id)
+        {
+            if (_context.Oferta == null)
+            {
+                _logger.LogError("Error: Oferta table does not exist");
+                return NotFound();
+            }
+
+            var oferta = await _context.Oferta
+                .Where (o => o.Id == id)
+                    .Include(o => o.ofertaItems)
+                        .ThenInclude(oi => oi.herramienta)
+                .Select(o => new OfertaDetailsDTO(
+                    o.Id,
+                    o.fechaInicio,
+                    o.fechaFinal,
+                    o.fechaOferta,
+                    o.metodoPago,
+                    o.dirigidaA,
+                    o.ofertaItems.Select(oi => new OfertaItemDTO(
+                        oi.ofertaId,
+                        oi.porcentaje,
+                        oi.precioFinal,
+                        oi.herramienta.Nombre,
+                        oi.herramienta.Material,
+                        oi.herramienta.fabricante.Nombre,
+                        oi.herramienta.Precio
+                    )).ToList<OfertaItemDTO>()
+                ))
+                .FirstOrDefaultAsync();
+
+            if (oferta == null)
+                {
+                _logger.LogError($"Error: Oferta with id { id} does not exist");
+                return NotFound();
+            }
+
+            return Ok(oferta);
+        }
+
+    }
+}
---
+++ src/AppForSEII2526.API/DTO/OfertaDTOs/OfertaDetailsDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.DTO.OfertaDTOs
+{
+    public class OfertaDetailsDTO: OfertaDTO
+    {
+        public OfertaDetailsDTO(int id, string fechaInicio, string fechaFinal, string fechaOferta,
+            tiposMetodoPago metodoPago, tiposDiridaOferta? dirigidaA, IList<OfertaItemDTO> ofertaItems):
+            base(id, fechaFinal, fechaInicio, fechaOferta, metodoPago, dirigidaA, ofertaItems)
+        {
+            this.id = id;
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.fechaOferta = fechaOferta;
+            this.metodoPago = metodoPago;
+            this.dirigidaA = dirigidaA;
+            this.ofertaItems = ofertaItems;
+        }
+
+        public int id { get; set; }
+
+        public string fechaInicio { get; set; }
+
+        public string fechaFinal { get; set; }
+        public string fechaOferta { get; set; }
+
+        public tiposMetodoPago metodoPago { get; set; }
+
+        public tiposDiridaOferta? dirigidaA { get; set; }
+
+        public IList<OfertaItemDTO> ofertaItems { get; set; }
+    }
+}
