2553e920f441e6a9322f92cf2370b7551d1b761b
telmocho <telmo.garcia@alu.uclm.es>
2025-10-28 14:49:29 +0100

    He organizado mis dtos en nuevas carpetas y he añadido el Get Select … (#65)
    He organizado mis dtos en nuevas carpetas y he añadido el Get Select en HerramientasController
    
    Co-authored-by: garci <garci@TELMO_PC>
    Co-authored-by: JaviTron2004 <javier.mellado1@alu.uclm.es>

--- src/AppForSEII2526.API/DTO/OfertaDTOs/OfertaDTO.cs
+++ src/AppForSEII2526.API/DTO/OfertaDTOs/OfertaDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace AppForSEII2526.API.DTO
+﻿namespace AppForSEII2526.API.DTO.OfertaDTOs
 {
     public class OfertaDTO
     {
@@ -20,11 +20,11 @@
         public int Id { get; set; }
 
         [Required]
-        public String fechaFinal { get; set; }
+        public string fechaFinal { get; set; }
 
         [Required]
-        public String fechaInicio { get; set; } // No puede ser anterior a hoy
-        public String fechaOferta { get; set; } // Debe ser después de la fecha de inicio y antes de la fecha final
+        public string fechaInicio { get; set; } // No puede ser anterior a hoy
+        public string fechaOferta { get; set; } // Debe ser después de la fecha de inicio y antes de la fecha final
 
         // Conexiones otras tablas
         public List<OfertaItem> ofertaItems { get; set; }
