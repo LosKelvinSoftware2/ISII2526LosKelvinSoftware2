60227d557549c7099495c427cd534631e4bcecd9
JuanPedroSerrano <juanpedro.serrano1@alu.uclm.es>
2025-10-28 19:14:57 +0100

    Creacion del post (#80)

--- src/AppForSEII2526.API/Models/Alquiler.cs
+++ src/AppForSEII2526.API/Models/Alquiler.cs
@@ -2,21 +2,34 @@
 {
     public class Alquiler
     {
+        public Alquiler(string direccionEnvio, DateTime fechaAlquiler, DateTime fechaFin, int id, float precioTotal, IList<AlquilarItem> alquilarItems, tiposMetodoPago metodoPago , string nombreCliente , string apellidoCliente)
+        {
+            this.direccionEnvio = direccionEnvio;
+            this.fechaAlquiler = fechaAlquiler;
+            this.fechaFin = fechaFin;
+            Id = id;
+            this.precioTotal = precioTotal;
+            AlquilarItems = alquilarItems;
+            MetodoPago = metodoPago;
+            this.nombreCliente= nombreCliente;
+            this.apellidoCliente= apellidoCliente;
+        }
 
         [Required]
         public String direccionEnvio { get; set; }
         [Required]
-        public String fechaAlquiler { get; set; }
+        public DateTime fechaAlquiler { get; set; }
         [Required]
-        public String fechaFin { get; set; }
+        public DateTime fechaFin { get; set; }
         [Key]
         public int Id { get; set; }
-        public String periodo { get; set; }
         [Range(0, float.MaxValue)]
         public float precioTotal { get; set; }
 
+        public string nombreCliente { get; set; }
+        public string apellidoCliente { get; set; }
+
         //Vinculación con el usuario (cliente autenticado)
-        [Required]
         public ApplicationUser Cliente { get; set; }
 
         [Required]
