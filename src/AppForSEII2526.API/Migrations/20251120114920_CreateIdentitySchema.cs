66d7333af36b4cb09bcaeade2cda5a408f36c7c9
JaviTron2004 <javier.mellado1@alu.uclm.es>
2025-11-20 13:16:55 +0100

    Cambio examen (#164)
    Co-authored-by: Guillermo Fernandez <GuillermoTomas.Fernandez@alu.uclm.es>

---
+++ src/AppForSEII2526.API/Migrations/20251120114920_CreateIdentitySchema.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppForSEII2526.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIdentitySchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "descripcion",
+                table: "Compra",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "descripcion",
+                table: "Compra");
+        }
+    }
+}
