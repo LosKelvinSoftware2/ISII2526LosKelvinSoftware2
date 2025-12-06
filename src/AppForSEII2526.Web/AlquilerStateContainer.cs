cc2fdc913aaabfca098aee17af35bc3d68fb8357
JuanPedroSerrano <juanpedro.serrano1@alu.uclm.es>
2025-12-07 00:45:52 +0100

    Ya funciona prácticamente mi interfaz del get select (#203)

---
+++ src/AppForSEII2526.Web/AlquilerStateContainer.cs
@@ -0,0 +1,55 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class AlquilerStateContainer
+    {
+        public AlquilerDTO Alquiler { get; private set; } = new AlquilerDTO()
+        {
+            AlquilarItems = new List<AlquilarItemDTO>()
+        };
+        public decimal PrecioTotal
+        {
+            get
+            {
+                int numberOfDays = (Alquiler.FechaFin - Alquiler.FechaInicio).Days;
+                return Convert.ToDecimal(Alquiler.AlquilarItems.Sum(ri => ri.Precio * numberOfDays));
+            }
+        }
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+        public void AddHerramientaToAlquiler(AlquilerHerramientasDTO alquiler)
+        {
+            if (!Alquiler.AlquilarItems.Any(ri => ri.NombreHerramienta == alquiler.Nombre))
+                Alquiler.AlquilarItems.Add(new AlquilarItemDTO()
+                {
+                    NombreHerramienta = alquiler.Nombre,
+                    MaterialHerramienta = alquiler.Material,
+                    Precio = alquiler.Precio,
+                    Cantidad = 1
+                }
+            );
+
+        }
+        public void RemoveAlquilarItemToAlquiler(AlquilarItemDTO item)
+        {
+            Alquiler.AlquilarItems.Remove(item);
+        }
+
+        public void ClearRentingCart()
+        {
+            Alquiler.AlquilarItems.Clear();
+        }
+        public void RentalProcessed()
+        {
+            Alquiler = new AlquilerDTO()
+            {
+                AlquilarItems = new List<AlquilarItemDTO>()
+            };
+        }
+    }
+}
