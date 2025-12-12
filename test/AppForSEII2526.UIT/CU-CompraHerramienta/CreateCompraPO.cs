01086d1a963843e00e20244c6a222b00439530ce
JaviTron2004 <javier.mellado1@alu.uclm.es>
2025-12-12 13:25:30 +0100

    Funcionan todas mis pruebas (Haciendo cambios en un DTO) (#230)

---
+++ test/AppForSEII2526.UIT/CU-CompraHerramienta/CreateCompraPO.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.CU_CompraHerramienta
+{
+    public class CreateCompraPO : PageObject
+    {
+        // IDs extraídos de CreateCompra.razor
+        private By _inputNombre = By.Id("Name");
+        private By _inputApellido = By.Id("Surname");
+        private By _inputCorreo = By.Id("Correo");
+        private By _inputDireccion = By.Id("DeliveryAddress");
+        private By _selectPago = By.Id("PaymentMethod");
+        private By _btnComprar = By.Id("Submit");
+
+        public CreateCompraPO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) { }
+
+        public void RellenarFormulario(string nombre, string apellido, string correo, string direccion, string metodoPago)
+        {
+            WaitForBeingVisible(_inputNombre);
+
+            _driver.FindElement(_inputNombre).Clear();
+            _driver.FindElement(_inputNombre).SendKeys(nombre);
+
+            _driver.FindElement(_inputApellido).Clear();
+            _driver.FindElement(_inputApellido).SendKeys(apellido);
+
+            _driver.FindElement(_inputCorreo).Clear();
+            _driver.FindElement(_inputCorreo).SendKeys(correo);
+
+            _driver.FindElement(_inputDireccion).Clear();
+            _driver.FindElement(_inputDireccion).SendKeys(direccion);
+
+            // Seleccionar en el desplegable
+            var selectElement = new SelectElement(_driver.FindElement(_selectPago));
+            selectElement.SelectByText(metodoPago);
+        }
+
+        public void EnviarFormulario()
+        {
+            WaitForBeingClickable(_btnComprar);
+            _driver.FindElement(_btnComprar).Click();
+        }
+
+        public void ConfirmarCompraEnModal()
+        {
+            // Usamos el método de PageObject para confirmar el diálogo
+            PressOkModalDialog();
+        }
+    }
+}
--- test/AppForSEII2526.UIT/Shared/UC_UIT.cs
+++ test/AppForSEII2526.UIT/Shared/UC_UIT.cs
@@ -3,15 +3,15 @@ using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 
 
-namespace AppForMovies.UIT.Shared {
+namespace AppForSEII2526.UIT.Shared {
     public class UC_UIT : IDisposable {
 
         private bool _pipeline = false;
 
         //establish which browser you would like to use
-        //private string _browser = "Chrome";
+        private string _browser = "Chrome";
         //private string _browser = "Firefox";
-        private string _browser = "Edge";
+        //private string _browser = "Edge";
 
         protected IWebDriver _driver;
         protected readonly ITestOutputHelper _output;
@@ -20,7 +20,7 @@ namespace AppForMovies.UIT.Shared {
         public string _URI {
             get {
                 //set url of your web page 
-                return "https://localhost:7083/";
+                return "https://localhost:7081/";
 
             }
         }
@@ -55,6 +55,7 @@ namespace AppForMovies.UIT.Shared {
                 .GoToUrl(_URI);
         }
 
+                 
         protected void Perform_login(string email, string password) {
             _driver.Navigate()
                     .GoToUrl(_URI + "Account/Login");
@@ -69,7 +70,7 @@ namespace AppForMovies.UIT.Shared {
             _driver.FindElement(By.XPath("/html/body/div[1]/main/article/div/div[1]/section/form/div[4]/button"))
                 .Click();
         }
-
+        
 
         protected void SetUp_Chrome4UIT() {
             var optionsc = new ChromeOptions {
