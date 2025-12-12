959c09ad9a69f950bcd07c5c4891fdfa543e4697
JuanPedroSerrano <juanpedro.serrano1@alu.uclm.es>
2025-12-12 17:29:25 +0100

    221 pruebas selenium select cu4 (#232)
    * Falta arreglar las pruebas
    
    * Pruebas del select arregladas
    
    * Ya me funcionan las pruebas del select
    
    ---------
    
    Co-authored-by: JaviTron2004 <javier.mellado1@alu.uclm.es>

--- test/AppForSEII2526.UIT/CU_Alquiler/UC_AlquilarHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/CU_Alquiler/UC_AlquilarHerramientas_UIT.cs
@@ -11,15 +11,15 @@ namespace AppForSEII2526.UIT.UC_Alquiler
     {
         private SelectHerramientasForAlquiler_PO selectHerramientasForAlquiler_PO;
 
-        private const string nombreHerramienta1 = "Taladro";
+        private const string nombreHerramienta1 = "Clavadora Neumática";
         private const string material1 = "Acero";
-        private const string precioHerramienta1 = "70";
-        private const string fabricante1 = "Bosch";
+        private const string precioHerramienta1 = "140";
+        private const string fabricante1 = "Kobalt";
 
-        private const string nombreHerramienta2 = "Martillo";
-        private const string material2 = "Madera";
-        private const string precioHerramienta2 = "30";
-        private const string fabricante2 = "Einhell";
+        private const string nombreHerramienta2 = "Cortadora de Azulejos";
+        private const string material2 = "Cerámica";
+        private const string precioHerramienta2 = "200";
+        private const string fabricante2 = "Hitachi";
         public UC_AlquilerHerramientas_UIT(ITestOutputHelper output) : base(output)
         {
             selectHerramientasForAlquiler_PO = new SelectHerramientasForAlquiler_PO(_driver, _output);
@@ -31,15 +31,37 @@ namespace AppForSEII2526.UIT.UC_Alquiler
             //we click on the menu
             _driver.FindElement(By.Id("CreateAlquiler")).Click();
         }
-        [Theory]
-        [InlineData(nombreHerramienta1, material1, fabricante1, precioHerramienta1, "Taladro", "")]
-        [InlineData(nombreHerramienta2, material2, fabricante2, precioHerramienta2, "", "Madera")]
+        [Fact]
         [Trait("LevelTesting", "Funcional Testing")]
-        public void CU4_filtering(string nombreHerramienta, string material, string fabricante, string precioHerramienta,
+        public void CU4_flujoPrincipalCompleto (){
+            //Arrange
+            Initial_step_opening_the_web_page();
+            InitialStepsForAlquilarHerramientas();
+
+
+        }
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]    
+        public void CU4_AF0_noHerramientasDisponibles()
+        {
+            //Arrange
+            Initial_step_opening_the_web_page();
+            InitialStepsForAlquilarHerramientas();
+            var expectedMessageError = "No hay herramientas disponibles con los filtros seleccionados.";
+            //Act
+            selectHerramientasForAlquiler_PO.BuscarHerramientas("herramienta_no_existente","herramienta_no_existente");
+            //Assert
+            Assert.True(selectHerramientasForAlquiler_PO.CheckMessageError(expectedMessageError));
+        }
+        [Theory]
+        [InlineData(nombreHerramienta1, material1, fabricante1, precioHerramienta1, "Clavadora", "")]
+        [InlineData(nombreHerramienta2, material2, fabricante2, precioHerramienta2, "", "Cerámica")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void CU4_AF1_filtradoPorNombreYMaterial(string nombreHerramienta, string material, string fabricante, string precioHerramienta,
             string filtroNombre, string filtroMaterial)
         {
-            Initial_step_opening_the_web_page();
             //Arrange
+            Initial_step_opening_the_web_page();
             InitialStepsForAlquilarHerramientas();
             var expectedHerramientas = new List<string[]> { new string[] { nombreHerramienta, material, fabricante, precioHerramienta }, };
 
@@ -49,7 +71,6 @@ namespace AppForSEII2526.UIT.UC_Alquiler
             //Assert
 
             Assert.True(selectHerramientasForAlquiler_PO.CheckListaHerramientas(expectedHerramientas));
-
         }
     }
 }
