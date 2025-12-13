5ffd3617755579b0b97c2c0b227506cdecbd2607
JUAN PEDRO SERRANO GARVI <JuanPedro.Serrano1@alu.uclm.es>
2025-12-13 14:04:08 +0100

    Ya me funcionan todas las pruebas del post

---
+++ test/AppForSEII2526.UIT/CU_Alquiler/CreateAlquiler_PO.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace AppForSEII2526.UIT.CU_Alquiler
+{
+    public class CreateAlquiler_PO : PageObject
+    {
+        By buttonModificarHerramientas = By.Id("ModificarAlquileres");
+        By tablaHerramientas = By.Id("TablaAlquilarItems");
+        By precioTotal = By.Id("precioTotal");
+        By nombreCliente = By.Id("NombreCliente");  
+        By apellidoCliente = By.Id("ApellidoCliente");
+        By direccionEnvio = By.Id("DireccionEnvio");    
+        By metodoPago = By.Id("MetodoPago");
+        By numTelefono = By.Id("NumTelefono");
+        By correoElectronico = By.Id("CorreoElectronico");
+        By fechaInicio = By.Id("FechaInicio");  
+        By fechaFin = By.Id("FechaFin");
+        By errorShownBy = By.Id("ErrorsShown");
+        By validationSummary = By.Id("formularioNoValido");
+        private By ErrorMessageLocator = By.CssSelector("#formularioNoValido li.validation-message");
+        public CreateAlquiler_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void ModificarHerramientas()
+        {
+            WaitForBeingClickable(buttonModificarHerramientas);
+            _driver.FindElement(buttonModificarHerramientas).Click();
+        }
+        public bool CheckListaHerramientas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, tablaHerramientas);
+        }
+        
+        public void RellenarDatos(string nombre, string apellido, string direccion, string metodoPay, string telefono, string correo, DateTime inicio, DateTime fin)
+        { 
+            WaitForBeingClickable(nombreCliente);
+            _driver.FindElement(nombreCliente).SendKeys(nombre);
+            WaitForBeingClickable(apellidoCliente);
+            _driver.FindElement(apellidoCliente).SendKeys(apellido);
+            WaitForBeingClickable(direccionEnvio);
+            _driver.FindElement(direccionEnvio).SendKeys(direccion);
+            WaitForBeingClickable(metodoPago);
+            new SelectElement(_driver.FindElement(metodoPago)).SelectByText(metodoPay);
+            WaitForBeingClickable(numTelefono);
+            _driver.FindElement(numTelefono).SendKeys(telefono);
+            WaitForBeingClickable(correoElectronico);
+            _driver.FindElement(correoElectronico).SendKeys(correo);
+            WaitForBeingClickable(fechaInicio);
+            InputDateInDatePicker(fechaInicio, inicio);
+            WaitForBeingClickable(fechaFin);
+            InputDateInDatePicker(fechaFin, fin);
+        }
+        public void enviarFormulario()
+        {
+            _driver.FindElement(By.Id("Confirmar")).Click();
+        }
+
+        public bool checkPrecioTotal(string expectedPrecioTotal)
+        {
+            IWebElement actualPrecioTotal = _driver.FindElement(precioTotal);
+            _output.WriteLine($"Precio total: {actualPrecioTotal.Text}");
+            return actualPrecioTotal.Text.Contains(expectedPrecioTotal);
+        }
+        public bool CheckMessageValidation(string errorMessage)
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(validationSummary));
+
+            IList<IWebElement> errorElements = _driver.FindElements(ErrorMessageLocator);
+            return errorElements.Any(e => e.Text.Trim() == errorMessage);
+        }
+        public bool CheckMessageError(string errorMessage)
+        {
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"Mensaje de error: {actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+        public void confirmarAlquiler()
+        {
+            _driver.FindElement(By.Id("Button_DialogOK")).Click();
+        }
+    }
+}
--- test/AppForSEII2526.UIT/CU_Alquiler/SelectHerramientasForAlquiler_PO.cs
+++ test/AppForSEII2526.UIT/CU_Alquiler/SelectHerramientasForAlquiler_PO.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
 
-namespace AppForSEII2526.UIT.UC_Alquiler
+namespace AppForSEII2526.UIT.CU_Alquiler
 {
     public class SelectHerramientasForAlquiler_PO : PageObject
     {
@@ -14,7 +14,7 @@ namespace AppForSEII2526.UIT.UC_Alquiler
         By buttonBuscarHerramientas = By.Id("buscarHerramientas");
         By tablaHerramientas = By.Id("tablaHerramientas");
         By errorShownBy = By.Id("ErrorsShown");
-        By buttonAlquilarHerramientas = By.Id("alquilarHerramientasButton");
+        By buttonAlquilarHerramientas = By.Id("alquilarHerramientaButton");
         public SelectHerramientasForAlquiler_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
         }
@@ -26,6 +26,7 @@ namespace AppForSEII2526.UIT.UC_Alquiler
             _driver.FindElement(inputMaterial).SendKeys(material);
             _driver.FindElement(buttonBuscarHerramientas).Click();
         }
+        
         public bool CheckListaHerramientas(List<string[]> expectedHerramientas)
         {
             return CheckBodyTable(expectedHerramientas, tablaHerramientas);
@@ -38,21 +39,28 @@ namespace AppForSEII2526.UIT.UC_Alquiler
         }
         public void AddHerramientaCarrito(string nombreHerramienta)
         {
+            WaitForBeingVisible(By.Id("addHerramienta_" + nombreHerramienta));
             WaitForBeingClickable(By.Id("addHerramienta_" + nombreHerramienta));
             _driver.FindElement(By.Id("addHerramienta_" + nombreHerramienta)).Click();
         }
 
-        public void RemoveMovieFromRentingCart(string nombreHerramienta)
+        public void RemoveAlquilerFromRentingCart(string nombreHerramienta)
         {
             WaitForBeingClickable(By.Id("removeHerramienta_" + nombreHerramienta));
-            _driver.FindElement(By.Id("removeMovie_" + nombreHerramienta)).Click();
+            _driver.FindElement(By.Id("removeHerramienta_" + nombreHerramienta)).Click();
         }
 
-        public bool RentingNotAvailable()
+        public bool CarritoVacio()
         {
-            //the button is not Displayed=hidden
+            IWebElement button = _driver.FindElement(buttonAlquilarHerramientas);
+            return !button.Enabled;
+        }
 
-            return _driver.FindElement(buttonAlquilarHerramientas).Displayed == false;
+        public void SeguirConAlquiler()
+        {
+            WaitForBeingVisible(buttonAlquilarHerramientas);
+            WaitForBeingClickable(buttonAlquilarHerramientas);
+            _driver.FindElement(buttonAlquilarHerramientas).Click();
         }
     }
 }
